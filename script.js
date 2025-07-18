e2e993713e02c07b830a7dd896b41d1df336dbd8
codewithosama03 <osamapathan03@gmail.com>
2025-07-18 14:41:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+// sidebar open
+function opensidebar() {
+ 
+ const sidebar =  document.querySelector(".sidebar");
+   sidebar.style.display = "flex "
+}
+
+// sidebar close
+function closesidebar() {
+    sidebar = document.querySelector(".sidebar");
+     sidebar.style.display = "none"
+}
+
